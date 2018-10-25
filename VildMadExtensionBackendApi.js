ea1a3a45a1b6bc3dbac43cb33d1126ca3a3b523f
Ruben Baskaran <rbaskaran92@gmail.com>
2018-10-25 12:31:43 +0200

    test

--- VildMadExtensionBackendApi.js
+++ VildMadExtensionBackendApi.js
@@ -14,7 +14,6 @@ const app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 
-
 // Routes
 const users = require('./routes/users');
 const ingredients = require('./routes/ingredients');
@@ -25,33 +24,25 @@ app.use('/ingredients', ingredients);
 app.use('/findings', findings);
 
 // Catch 404 Errors and forward them to error handler
-// app.use((req, res, next) => {
-//     const err = new Error('Not Found');
-//     err.status = 404;
-//     next(err);
-// });
-
-// Error Handler function 
-// app.use((err, req, res, next) => {
-//     const error = app.get('env') === 'development' ? err : {};
-//     const status = err.status || 500;
-
-//     res.status(status).json({
-//         error: {
-//             message: error.message
-//         }
-//     });
-
-//     console.error(err);
-// });
-
-app.get("/", function(req, res){
-    res.send("Welcome to Vild Mad Extension Backend API");
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-app.get("*", function(req, res){
-    res.send("This is the default route for Vild Mad Extension Backend API");
-}); 
+// Error Handler function 
+app.use((err, req, res, next) => {
+    const error = app.get('env') === 'development' ? err : {};
+    const status = err.status || 500;
+
+    res.status(status).json({
+        error: {
+            message: error.message
+        }
+    });
+
+    console.error(err);
+});
 
 // Start server
 const port = process.env.PORT || 3000;
